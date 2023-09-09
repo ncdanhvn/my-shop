cc954ec96972a47651934e7664b8b2d7089b4c97
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-09 22:14:28 +0700

    Connect tags to shop admin,

--- shop_custom/admin.py
+++ shop_custom/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from shop.admin import ProductAdmin
+from tags.models import TaggedItem
+from shop.models import Product
 
-# Register your models here.
+
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
