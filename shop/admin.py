d22778c761ad488caf95c416de61a67f4a8c835b
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-09 21:39:58 +0700

    Setup the admin panel page

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Collection)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'collection_title']
+    list_editable = ['price']
+    list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'payment_status', 'customer']
+    list_editable = ['payment_status']
+    list_per_page = 10
+    list_select_related = ['customer']    
--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -13,6 +19,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     slug = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
