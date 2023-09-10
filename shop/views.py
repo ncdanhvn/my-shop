6e203f4c8de7eae0e53a31a7a38a6ebb73f6a89b
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-10 20:21:57 +0700

    Apply filter, search, ordering and pagination for product list endpoint

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from .models import Product, Collection, OrderItem
 from .serializers import CollectionSerializer, ProductSerializer
+from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection')
     serializer_class = ProductSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['price', 'inventory']
 
     def get_serializer_context(self):
         return {'request': self.request}
