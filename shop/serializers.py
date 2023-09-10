6673529e34623bdda3bd280003273c2c4d292f26
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-10 10:29:25 +0700

    Build Collection and Product get method by using ModelSerializer

---
+++ shop/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Product, Collection
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'collection']
+
+    collection = CollectionSerializer()
--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product, Collection
+from .serializers import CollectionSerializer, ProductSerializer
 
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request': request})
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
