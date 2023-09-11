cf3a504e190a511a4528df1e407fec2197259616
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-11 20:23:29 +0700

    Log in user and get current user

--- myshop/settings.py
+++ myshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,10 +148,12 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 DJOSER = {
     'SERIALIZERS': {
-        'user_create': 'shop_custom.serializers.UserCreateSerializer'
+        'user_create': 'shop_custom.serializers.UserCreateSerializer',
+        'current_user': 'shop_custom.serializers.UserSerializer',
     }
 }
--- shop_custom/serializers.py
+++ shop_custom/serializers.py
@@ -1,7 +1,13 @@
-from djoser.serializers import UserCreateSerializer
+from djoser.serializers import UserSerializer, UserCreateSerializer
 
 
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
+                  'email', 'first_name', 'last_name']
+        
+
+class UserSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = ['id', 'username',
                   'email', 'first_name', 'last_name']
